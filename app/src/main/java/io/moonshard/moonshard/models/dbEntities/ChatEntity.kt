d990c798db7a72aacdff876e5bba78c776b0cfbb
ChronosX88 <chronosx88@gmail.com>
2019-12-02 19:05:41 +0400

    fix: Add @Unique annotation to ChatEntity and ChatUser model

--- app/src/main/java/io/moonshard/moonshard/models/dbEntities/ChatEntity.kt
+++ app/src/main/java/io/moonshard/moonshard/models/dbEntities/ChatEntity.kt
@@ -3,12 +3,13 @@ package io.moonshard.moonshard.models.dbEntities
 import io.objectbox.annotation.Backlink
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.annotation.Unique
 import io.objectbox.relation.ToMany
 
 @Entity
 data class ChatEntity(
     @Id var id: Long = 0,
-    var jid: String,
+    @Unique var jid: String,
     var chatName: String = "",
     var isGroupChat: Boolean = false,
     var unreadMessagesCount: Int = 0
--- app/src/main/java/io/moonshard/moonshard/models/dbEntities/ChatUser.kt
+++ app/src/main/java/io/moonshard/moonshard/models/dbEntities/ChatUser.kt
@@ -2,11 +2,12 @@ package io.moonshard.moonshard.models.dbEntities
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.annotation.Unique
 
 @Entity
 data class ChatUser(
     @Id var id: Long = 0,
-    val jid: String = "",
+    @Unique val jid: String = "",
     val name: String = "",
     var lastSeen: Long = -1, // how long user is idle
     var isOnline: Boolean = false

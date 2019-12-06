76a3a9374007d7f591fe2e800a630be2f8147892
Tony <tonitonytoni11@gmail.com>
2019-12-06 12:23:15 +0200

    feat: implement settings screen with real data

--- app/src/main/java/io/moonshard/moonshard/presentation/view/SettingsView.kt
+++ app/src/main/java/io/moonshard/moonshard/presentation/view/SettingsView.kt
@@ -1,8 +1,11 @@
 package io.moonshard.moonshard.presentation.view
 
+import android.graphics.Bitmap
 import moxy.MvpView
 
 interface SettingsView : MvpView {
     fun showRegistrationScreen()
     fun showError(error:String)
+    fun setAvatar(avatar: Bitmap?)
+    fun setData(nickName:String?,jidPart:String?)
 }
--- app/src/main/java/io/moonshard/moonshard/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/io/moonshard/moonshard/ui/fragments/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package io.moonshard.moonshard.ui.fragments.settings
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import io.moonshard.moonshard.presentation.presenter.SettingsPresenter
 import io.moonshard.moonshard.presentation.view.SettingsView
 import io.moonshard.moonshard.ui.activities.RegisterActivity
 import io.moonshard.moonshard.ui.fragments.create_group.CreateNewChatFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 import moxy.MvpAppCompatFragment
 import moxy.presenter.InjectPresenter
@@ -31,6 +33,9 @@ class SettingsFragment : MvpAppCompatFragment(), SettingsView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        presenter.getAvatar()
+        presenter.getName()
         logOut?.setOnClickListener {
             presenter.logOut()
             clearLoginCredentials()
@@ -39,6 +44,11 @@ class SettingsFragment : MvpAppCompatFragment(), SettingsView {
         profileSettingsLayout?.setOnClickListener {
             showProfileScreen()
         }
+    }
+
+    override fun setData(nickName: String?, jidPart: String?) {
+        nameTv?.text = nickName ?: "Имя"
+        phoneTv?.text = jidPart ?: "jid"
     }
 
     override fun showRegistrationScreen() {
@@ -62,6 +72,10 @@ class SettingsFragment : MvpAppCompatFragment(), SettingsView {
         SecurePreferences.setValue("jid", "")
         SecurePreferences.setValue("pass", "")
         SecurePreferences.setValue("logged_in", false)
+    }
+
+    override fun setAvatar(avatar: Bitmap?) {
+        avatarIv?.setImageBitmap(avatar)
     }
 
 }

91acad1dbe4ddf5a17a153270e592d3884023d87
ChronosX88 <chronosx88@gmail.com>
2019-12-11 17:32:44 +0400

    fix: Make messageUid unique (prevent message duplicating)

--- app/src/main/java/io/moonshard/moonshard/models/dbEntities/MessageEntity.kt
+++ app/src/main/java/io/moonshard/moonshard/models/dbEntities/MessageEntity.kt
@@ -2,12 +2,13 @@ package io.moonshard.moonshard.models.dbEntities
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.annotation.Unique
 import io.objectbox.relation.ToOne
 
 @Entity
 data class MessageEntity(
     @Id var id: Long = 0,
-    var messageUid: String = "",
+    @Unique var messageUid: String = "",
     var timestamp: Long = -1,
     var text: String = "",
     var isSent: Boolean = false,

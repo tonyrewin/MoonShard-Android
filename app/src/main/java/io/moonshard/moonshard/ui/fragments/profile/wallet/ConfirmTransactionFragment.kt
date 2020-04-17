bea040654b646e77630e1def8eaceb5a89a4c1e6
Tony <tonitonytoni11@gmail.com>
2020-04-17 11:29:47 +0200

    feat: implement new screen: confirm transaction screen

---
+++ app/src/main/java/io/moonshard/moonshard/ui/fragments/profile/wallet/ConfirmTransactionFragment.kt
@@ -0,0 +1,43 @@
+package io.moonshard.moonshard.ui.fragments.profile.wallet
+
+import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import io.moonshard.moonshard.R
+import io.moonshard.moonshard.common.utils.setSafeOnClickListener
+import kotlinx.android.synthetic.main.fragment_confirm_transaction.*
+
+
+class ConfirmTransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirm_transaction, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var isSecurity = true
+        visiblePassBtn?.setSafeOnClickListener {
+            if (isSecurity) {
+                editPassword?.transformationMethod = null
+                visiblePassBtn?.setImageResource(R.drawable.ic_pass_on)
+                isSecurity = false
+            } else {
+                editPassword?.transformationMethod = PasswordTransformationMethod()
+                visiblePassBtn?.setImageResource(R.drawable.ic_pass_off)
+                isSecurity = true
+            }
+        }
+
+
+    }
+}
